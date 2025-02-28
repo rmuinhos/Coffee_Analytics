447dd807617195c24ffedc1cb628048139ccba3c
Muinhos <rmuinhos@gmail.com>
2025-02-28 09:52:51 -0300

    Comments fixed

--- coffee_analytics_conab.py
+++ coffee_analytics_conab.py
@@ -3,7 +3,7 @@ Coffee Analitcs - Companhia Nacional de Abastecimento (CONAB)
 Autor: Ronaldo Muinhos
 Data: 2025-02-20
 Descrição: Análise de dados com Regressão Linear para dados de
-produção de café no mundo, utilizando dados da CONAB
+produção de café no Brasil, utilizando dados da CONAB
 Fonte dos Dados: https://www.conab.gov.br/info-agro/safras/serie-historica-das-safras
 Tipo: Arábica + Conilon
 Instruções Excel: Deixar apenas "Brasil", Remover cabeçalhos e rodapés. Exportar para csv utf-8 (sepadador ";")
